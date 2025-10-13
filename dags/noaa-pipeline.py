4b20862d32f37399868a9a25ffb51133092cf648
ArtemSaakov <asaakov@umich.edu>
2025-10-13 17:43:43 -0400

    beginning dags

--- dags/noaa-pipeline.py
+++ dags/noaa-pipeline.py
@@ -1,2 +1,37 @@
 import requests as req
+from requests.exceptions import HTTPError
 import pandas as pd
+import time
+from http import HTTPStatus
+
+
+ENDPOINT = "https://api.weather.gov/stations/"
+DTW = "KDTW"
+ERRORS = [
+    HTTPStatus.TOO_MANY_REQUESTS,
+    HTTPStatus.INTERNAL_SERVER_ERROR,
+    HTTPStatus.BAD_GATEWAY,
+    HTTPStatus.SERVICE_UNAVAILABLE,
+    HTTPStatus.GATEWAY_TIMEOUT,
+]
+RETRIES = 3
+
+
+def fetch_data(station_id: str = DTW):
+    url = f"{ENDPOINT}{station_id}/observations/latest"
+
+    for n in range(RETRIES):
+        try:
+            response = req.get(url, headers={"Accept": "application/ld+json"})
+            response.raise_for_status()
+            break
+
+        except HTTPError as exc:
+            code = exc.response.status_code
+            # rudimentary exponential backoff
+            if code in ERRORS:
+                # aka 2-4-8 second delays
+                time.sleep(2 ** (n + 1))
+                continue
+
+            raise
